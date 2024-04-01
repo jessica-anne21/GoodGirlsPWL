57a375b4f51f0660b2776c3b754513da668001a0
jessica-anne21 <113569098+jessica-anne21@users.noreply.github.com>
2024-04-01 17:32:47 +0700

    Delete show.blade.php

--- show.blade.php
+++
@@ -1,9 +0,0 @@
-<?php
-@extends('layouts.app')
-
-@section('content')
-    <h1>Informasi User</h1>
-    <p>Nama: {{ $user->name }}</p>
-    <p>Email: {{ $user->email }}</p>
-    <p>Created at: {{ $user->created_at }}</p>
-@endsection

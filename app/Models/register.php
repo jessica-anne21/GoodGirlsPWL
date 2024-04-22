2ab471979a2007e557a471d12e00ff38b313c176
lalanabiyaa <octavianinabilla123@gmail.com>
2024-04-22 20:40:51 +0700

    fitur untuk password

--- app/Models/register.php
+++ app/Models/register.php
@@ -1,13 +1,6 @@
 <?php
 
 namespace App\Models;
-
-class resgister
-{
-
-
-namespace App\Models;
-
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
 
